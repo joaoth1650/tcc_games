6fc9dfdfdb17eb82e4c85aa4e1d548702095060e
theobaldinho <joaopedromcj9@gmail.com>
2023-09-20 17:00:50 -0300

    klk

--- resources/js/Pages/Games/IndexGames.tsx
+++ resources/js/Pages/Games/IndexGames.tsx
@@ -12,15 +12,15 @@ export default function IndexGames({ auth, games }: PageProps<{ games: any }>) {
           <div className=" overflow-hidden shadow-sm rounded-lg grid grid-cols-3 gap-5">
             {games.map((game: any) => (
               <Link href={route('games.show', { 'id': game.id })}
-                className="ml-4 font-semibold text-gray-500 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                className="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
               >
-                <div key={game.id} className=" bg-white rounded-lg hover:bg-slate-200  border-gray-500 w-">
+                <div key={game.id} className=" bg-gray-300 rounded-lg hover:bg-slate-100  border-gray-700">
                   <img src={game.imagem_principal} alt="" className={"border object-cover rounded-lg shadow-md"} />
 
                   <h1 className="text-2xl text-center font-bold uppercase">{game.nome}</h1>
                   
                   <p className="leading-4 px-2">{game.descricao.substring(0, 50) + '...'}     </p>
-                  <h2>{game.preco}</h2>
+                  <h2 className="px-2">R${game.preco}</h2>
 
                 </div>
               </Link>
